474e63d2505b4b86fbfae6400785db1c2d1d5fc0
glaslos <glaslos@gmail.com>
2013-05-03 12:54:21 +0200

    setup cleaned up

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+sourcefiles = ['src/pylibinjection.pyx']
+
+
+setup(
+    name="pylibinjection",
+    packages=[],
+    version="0.1",
+    description="Libinjection Python wrapper",
+    url="",
+    author="Angelo Dell'Aera",
+    author_email="angelo.dellaera@honeynet.org",
+    classifiers=[
+        "Programming Language :: Cython",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: Unix",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Security",
+    ],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("pylibinjection",
+                           sourcefiles,
+                           include_dirs=["/opt/libinjection/c"],
+                           library_dirs=["/opt/libinjection/c"],
+                           #libraries=["is_sqli"]
+                           )
+    ],
+)
